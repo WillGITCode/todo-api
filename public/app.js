7ac0dc4e41d2c4030e243af0fa750ec4ba0a4128
William S <25758160+WillGITCode@users.noreply.github.com>
2018-03-01 21:15:42 -0500

    add get request for todos through jQ and show on page

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
-alert("Hi from app.js");
+$(document).ready(function(){
+    $.getJSON("/api/todos")
+    .then(function(data){
+        console.log(data);
+    })
+});
+
+function addTodos(todos){
+    //add todos to page here
+    todos.forEach(function(todo){
+        var newTodo = $("<li class='task'>" + todo.name + "</li>");
+        $(".list").append(newTodo);
+    });
+}
