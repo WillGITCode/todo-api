0cd97b78272d09edd8b9c993d5bb174229f0b794
William S <25758160+WillGITCode@users.noreply.github.com>
2018-03-01 21:32:42 -0500

    add functions for getting user input add adding to DB, update page and clear input

--- public/app.js
+++ public/app.js
@@ -1,14 +1,37 @@
 $(document).ready(function(){
     $.getJSON("/api/todos")
-    .then(function(data){
-        console.log(data);
-    })
+    .then(addTodos)
+
+    $("#todoInput").keypress(function(event){
+        if(event.which == 13){
+            createTodo();
+        }
+    });
 });
 
 function addTodos(todos){
     //add todos to page here
     todos.forEach(function(todo){
-        var newTodo = $("<li class='task'>" + todo.name + "</li>");
-        $(".list").append(newTodo);
+       addTodo(todo);
     });
+}
+
+function addTodo(todo){
+    var newTodo = $("<li class='task'>" + todo.name + "</li>");
+    if(todo.completed){
+        newTodo.addClass("done");
+    }
+    $(".list").append(newTodo);
+}
+function createTodo(){
+    // send post request with user input to /api/todos
+    var userInput = $("#todoInput").val();
+    $.post("/api/todos", {name: userInput})
+    .then(function(newTodo){
+        $("#todoInput").val("");
+        addTodo(newTodo);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 }
